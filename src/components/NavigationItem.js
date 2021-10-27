19b1d30f32915791f969710171aa1cf8bf626541
John W. Martin <36454057+jmartin103@users.noreply.github.com>
2021-10-27 16:10:38 -0400

    styling

--- src/components/NavigationItem.js
+++ src/components/NavigationItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavigationItem = (props) => (
-  <div class="NavigationItem">
+  <div className="NavigationItem">
     <Link to={props.destination}>{props.name}</Link>
   </div>
 );
